359645eceef887427b396cbadae7beded30dd273
Dreamergirly81 <alberti.tamara@gmail.com>
2022-07-04 23:15:10 +0200

    add static wrapper Method and List of ratings

--- src/main/java/de/makitama/pokerapp/ranking/Rank.java
+++ src/main/java/de/makitama/pokerapp/ranking/Rank.java
@@ -1,22 +1,40 @@
 package de.makitama.pokerapp.ranking;
 
-public class Rank {
-    private final Ranks type;
+import java.util.ArrayList;
+import java.util.List;
 
-    private Rank (RankBuilder rankBuilder) {
-        this.type = rankBuilder.type;
-    }
+public class Rank {
+
+    private final Ranks type;
+    private final List<Integer> ratings = new ArrayList<>();
 
     public static class RankBuilder {
         private final Ranks type;
+        private final List<Integer> ratings = new ArrayList<>();
 
         public RankBuilder(Ranks type) {
             this.type = type;
         }
 
+        public RankBuilder addRating(int rating) {
+            ratings.add(rating);
+            return this;
+        }
+
         public Rank build() {
             return new Rank (this);
         }
+    }
+
+
+
+    private Rank (RankBuilder rankBuilder) {
+        this.type = rankBuilder.type;
+        this.ratings.addAll(rankBuilder.ratings);
+    }
+
+    public static RankBuilder initiateRankingFor(Ranks type) {
+        return new RankBuilder(type);
     }
 
 }
