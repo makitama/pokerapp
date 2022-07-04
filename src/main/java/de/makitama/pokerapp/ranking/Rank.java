7c7be2d68123b76fb0677a5af705a19a3622fa74
Dreamergirly81 <alberti.tamara@gmail.com>
2022-07-05 00:13:36 +0200

    add compare Method to Rank

--- src/main/java/de/makitama/pokerapp/ranking/Rank.java
+++ src/main/java/de/makitama/pokerapp/ranking/Rank.java
@@ -1,6 +1,7 @@
 package de.makitama.pokerapp.ranking;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Rank {
@@ -26,8 +27,6 @@ public class Rank {
         }
     }
 
-
-
     private Rank (RankBuilder rankBuilder) {
         this.type = rankBuilder.type;
         this.ratings.addAll(rankBuilder.ratings);
@@ -35,6 +34,44 @@ public class Rank {
 
     public static RankBuilder initiateRankingFor(Ranks type) {
         return new RankBuilder(type);
+    }
+
+    /**
+     *
+     * @param rank - The Rank to be compared
+     * @return   0 => both Ranks are equal
+     *           1 => this object is ranked higher (aka is Winner)
+     *          -1 => argument Rank is ranked higher (aka is Winner)
+     */
+    public int compareTo(Rank rank) {
+        int winnerOfType = compareRanks(rank);
+        if(winnerOfType == 0) {
+            return compareValues(rank);
+        }
+        return winnerOfType;
+    }
+
+    public int compareRanks(Rank rank) {
+        return this.type.compareTo(rank.type);
+    }
+
+    public int compareValues(Rank rank) {
+        Iterator<Integer> thisIterater = this.ratings.iterator();
+        Iterator<Integer> argumentIterater = rank.ratings.iterator();
+
+        while (thisIterater.hasNext()) {
+            if (!argumentIterater.hasNext()) {
+                throw new IllegalArgumentException("List of ratings of 'Other Rank' (" + rank + ") is smaller than this List");
+            }
+            Integer v1 = thisIterater.next();
+            Integer v2 = argumentIterater.next();
+
+            int winner = v1.compareTo(v2);
+            if (winner != 0) {
+                return winner;
+            }
+        }
+        return 0;
     }
 
 }
