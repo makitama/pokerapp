cd29033f76f22372482deda874ee574f39938fc2
Dreamergirly81 <alberti.tamara@gmail.com>
2022-07-05 23:35:19 +0200

    implement four_of_a_kind rule & test

---
+++ src/main/java/de/makitama/pokerapp/rules/FourOfAKind.java
@@ -0,0 +1,39 @@
+package de.makitama.pokerapp.rules;
+
+import de.makitama.pokerapp.cards.Card;
+import de.makitama.pokerapp.ranking.HandRankings;
+import de.makitama.pokerapp.ranking.Rank;
+import de.makitama.pokerapp.services.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+public class FourOfAKind implements Rule{
+
+    private final HandRankings handRanking = HandRankings.FOUR_OF_A_KIND;
+
+    /*
+     * 4 cards with the same value. Ranked by the value of the 4 cards.
+     */
+
+    public static boolean isFourOfAKind(List<Card> hand) {
+        return Service.isDistinctCardValueEqualsToGivenAmount(hand, 2);
+    }
+
+   public static List<Card> getCardsForQuadruple(List<Card> hand) {
+        return Service.getCardsWithDuplicatesValues(hand, 4);
+   }
+
+    @Override
+    public Optional<Rank> rank(List<Card> hand) {
+        if(isFourOfAKind(hand)) {
+            Rank.RankBuilder rankBuilder = Rank.initiateRankingFor(handRanking);
+            int worthOfQuadruple = getCardsForQuadruple(hand).stream().mapToInt(card -> card.getValue().getRating()).sum();
+            rankBuilder.addRating(worthOfQuadruple);
+            return Optional.of(rankBuilder.build());
+        }
+        return Optional.empty();
+    }
+}
---
+++ src/test/java/de/makitama/pokerapp/rules/FourOfAKindTest.java
@@ -0,0 +1,44 @@
+package de.makitama.pokerapp.rules;
+
+import de.makitama.pokerapp.cards.Card;
+import de.makitama.pokerapp.cards.CardSuit;
+import de.makitama.pokerapp.cards.CardValue;
+import de.makitama.pokerapp.ranking.HandRankings;
+import de.makitama.pokerapp.ranking.Rank;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class FourOfAKindTest {
+
+	@Mock
+	Rank rank;
+
+	Card card1 = new Card(CardSuit.C, CardValue._8);
+	Card card2 = new Card(CardSuit.H, CardValue._8);
+	Card card3 = new Card(CardSuit.D, CardValue._8);
+	Card card4 = new Card(CardSuit.S, CardValue._8);
+	Card card5 = new Card(CardSuit.C, CardValue._3);
+
+	private final List<Card> hand = List.of(card5, card2, card1, card3, card4);
+
+	@Test
+	void testRank() {
+		assertNotNull(rank);
+		when(rank.getType()).thenReturn(HandRankings.FOUR_OF_A_KIND);
+		when(rank.getRatings()).thenReturn(List.of(card4.getValue().getRating()*4));
+		FourOfAKind fourOfAKind = new FourOfAKind();
+
+		assertTrue(fourOfAKind.rank(hand).isPresent());
+		assertEquals(rank.getRatings(), fourOfAKind.rank(hand).get().getRatings());
+		assertEquals(rank.getType(), fourOfAKind.rank(hand).get().getType());
+	}
+}
