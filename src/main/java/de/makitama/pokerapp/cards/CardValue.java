e7f572e77fae7569dd9b299e4daecd5f9e192d48
Dreamergirly81 <alberti.tamara@gmail.com>
2022-07-05 23:16:33 +0200

    change values of Cards

--- src/main/java/de/makitama/pokerapp/cards/CardValue.java
+++ src/main/java/de/makitama/pokerapp/cards/CardValue.java
@@ -2,23 +2,23 @@ package de.makitama.pokerapp.cards;
 
 public enum CardValue {
 
-	_2(1), 
-	_3(2),
-	_4(3),
-	_5(4),
-	_6(5),
-	_7(6),
-	_8(7),
-	_9(8),
-	_T(9),
-	_J(10),
-	_Q(11),
-	_K(12),
-	_A(13);
+	_2(2),
+	_3(3),
+	_4(4),
+	_5(5),
+	_6(6),
+	_7(7),
+	_8(8),
+	_9(9),
+	_T(10),
+	_J(11),
+	_Q(12),
+	_K(13),
+	_A(14);
 	
 	public final int value;
 	
-	private CardValue (int value) {
+	CardValue (int value) {
 		this.value = value;
 	}
 	
