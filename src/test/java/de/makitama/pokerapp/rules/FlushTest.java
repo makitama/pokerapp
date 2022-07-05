9f52e76909b1d242c2283348e910997a05534f10
Dreamergirly81 <alberti.tamara@gmail.com>
2022-07-05 23:34:38 +0200

    add basic test for rank Method

--- src/test/java/de/makitama/pokerapp/rules/FlushTest.java
+++ src/test/java/de/makitama/pokerapp/rules/FlushTest.java
@@ -3,12 +3,19 @@ package de.makitama.pokerapp.rules;
 import de.makitama.pokerapp.cards.Card;
 import de.makitama.pokerapp.cards.CardSuit;
 import de.makitama.pokerapp.cards.CardValue;
+import de.makitama.pokerapp.ranking.HandRankings;
+import de.makitama.pokerapp.ranking.Rank;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class FlushTest {
 
     Card card1 = new Card(CardSuit.C, CardValue._8);
@@ -20,15 +27,31 @@ class FlushTest {
     private final List<Card> hand = List.of(card2, card5, card1, card3, card4);
 
     @Test
-    void FlushShouldBeFound() {
+    void flushShouldBeFound() {
         assertTrue(Flush.isFlush(hand));
     }
 
     @Test
-    void FlushShouldNotBeFound() {
+    void flushShouldNotBeFound() {
         card3 =new Card(CardSuit.D, CardValue._8);
         List<Card> hand = List.of(card2, card5, card1, card3, card4);
         assertFalse(Flush.isFlush(hand));
+    }
+
+
+    @Mock
+    Rank rank;
+
+    @Test
+    void testRank() {
+        assertNotNull(rank);
+        when(rank.getType()).thenReturn(HandRankings.FLUSH);
+        when(rank.getRatings()).thenReturn(List.of(card4.getValue().getRating(), card3.getValue().getRating(), card1.getValue().getRating(), card5.getValue().getRating(), card2.getValue().getRating()));
+        Flush flush = new Flush();
+
+        assertTrue(flush.rank(hand).isPresent());
+        assertEquals(rank.getRatings(), flush.rank(hand).get().getRatings());
+        assertEquals(rank.getType(), flush.rank(hand).get().getType());
     }
 
 }
