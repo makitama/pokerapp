9f23896dc5626f1d8fb4c437d9074f8014fd6809
Dreamergirly81 <alberti.tamara@gmail.com>
2022-07-05 23:22:08 +0200

    implement Interface and add Rank Method to HighCard

--- src/main/java/de/makitama/pokerapp/rules/HighCard.java
+++ src/main/java/de/makitama/pokerapp/rules/HighCard.java
@@ -1,23 +1,37 @@
 package de.makitama.pokerapp.rules;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 import de.makitama.pokerapp.cards.Card;
+import de.makitama.pokerapp.ranking.HandRankings;
+import de.makitama.pokerapp.ranking.Rank;
 
 public class HighCard implements Rule{
 
+	private final HandRankings handRanking = HandRankings.HIGH_CARD;
 	/*
 	 * Hands which do not fit any higher category are ranked by the value of their highest card.
 	 * If the highest cards have the same value, the hands are ranked by the next highest, and so on.
 	 */
 
-
 	public static Card getHighestCard(List<Card> hand) {
 		//TODO Check isPresent() => Optional. => necessary?
-		return hand.stream().max(Comparator.comparing(Card::getValue)).get();
-	}
-//TODO HighCard => nexthighest etc..
 
+		Optional<Card> card = hand.stream().max(Comparator.comparing(Card::getValue));
+		return (card.orElse(null));
+	}
+
+	@Override
+	public Optional<Rank> rank(List<Card> hand) {
+
+		Rank.RankBuilder rankBuilder = Rank.initiateRankingFor(handRanking);
+		Collections.reverse(hand);
+		hand.forEach(card -> rankBuilder.addRating(card.getValue().getRating()));
+
+		return Optional.of(rankBuilder.build());
+	}
 
 }
