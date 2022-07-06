967d2a65630927b92c4bd65a7f5c8e9ff1fec2ee
Dreamergirly81 <alberti.tamara@gmail.com>
2022-07-06 22:44:29 +0200

    add class

---
+++ src/test/java/de/makitama/pokerapp/PredefinedCards.java
@@ -0,0 +1,65 @@
+package de.makitama.pokerapp;
+
+import de.makitama.pokerapp.cards.Card;
+import de.makitama.pokerapp.cards.CardSuit;
+import de.makitama.pokerapp.cards.CardValue;
+
+public class PredefinedCards {
+
+    public static final Card CARD_C2 = new Card(CardSuit.C, CardValue._2);
+    public static final Card CARD_C3 = new Card(CardSuit.C, CardValue._3);
+    public static final Card CARD_C4 = new Card(CardSuit.C, CardValue._4);
+    public static final Card CARD_C5 = new Card(CardSuit.C, CardValue._5);
+    public static final Card CARD_C6 = new Card(CardSuit.C, CardValue._6);
+    public static final Card CARD_C7 = new Card(CardSuit.C, CardValue._7);
+    public static final Card CARD_C8 = new Card(CardSuit.C, CardValue._8);
+    public static final Card CARD_C9 = new Card(CardSuit.C, CardValue._9);
+    public static final Card CARD_CT = new Card(CardSuit.C, CardValue._T);
+    public static final Card CARD_CJ = new Card(CardSuit.C, CardValue._J);
+    public static final Card CARD_CQ = new Card(CardSuit.C, CardValue._Q);
+    public static final Card CARD_CK = new Card(CardSuit.C, CardValue._K);
+    public static final Card CARD_CA = new Card(CardSuit.C, CardValue._A);
+
+    public static final Card CARD_D2 = new Card(CardSuit.D, CardValue._2);
+    public static final Card CARD_D3 = new Card(CardSuit.D, CardValue._3);
+    public static final Card CARD_D4 = new Card(CardSuit.D, CardValue._4);
+    public static final Card CARD_D5 = new Card(CardSuit.D, CardValue._5);
+    public static final Card CARD_D6 = new Card(CardSuit.D, CardValue._6);
+    public static final Card CARD_D7 = new Card(CardSuit.D, CardValue._7);
+    public static final Card CARD_D8 = new Card(CardSuit.D, CardValue._8);
+    public static final Card CARD_D9 = new Card(CardSuit.D, CardValue._9);
+    public static final Card CARD_DT = new Card(CardSuit.D, CardValue._T);
+    public static final Card CARD_DJ = new Card(CardSuit.D, CardValue._J);
+    public static final Card CARD_DQ = new Card(CardSuit.D, CardValue._Q);
+    public static final Card CARD_DK = new Card(CardSuit.D, CardValue._K);
+    public static final Card CARD_DA = new Card(CardSuit.D, CardValue._A);
+
+    public static final Card CARD_H2 = new Card(CardSuit.H, CardValue._2);
+    public static final Card CARD_H3 = new Card(CardSuit.H, CardValue._3);
+    public static final Card CARD_H4 = new Card(CardSuit.H, CardValue._4);
+    public static final Card CARD_H5 = new Card(CardSuit.H, CardValue._5);
+    public static final Card CARD_H6 = new Card(CardSuit.H, CardValue._6);
+    public static final Card CARD_H7 = new Card(CardSuit.H, CardValue._7);
+    public static final Card CARD_H8 = new Card(CardSuit.H, CardValue._8);
+    public static final Card CARD_H9 = new Card(CardSuit.H, CardValue._9);
+    public static final Card CARD_HT = new Card(CardSuit.H, CardValue._T);
+    public static final Card CARD_HJ = new Card(CardSuit.H, CardValue._J);
+    public static final Card CARD_HQ = new Card(CardSuit.H, CardValue._Q);
+    public static final Card CARD_HK = new Card(CardSuit.H, CardValue._K);
+    public static final Card CARD_HA = new Card(CardSuit.H, CardValue._A);
+
+    public static final Card CARD_S2 = new Card(CardSuit.S, CardValue._2);
+    public static final Card CARD_S3 = new Card(CardSuit.S, CardValue._3);
+    public static final Card CARD_S4 = new Card(CardSuit.S, CardValue._4);
+    public static final Card CARD_S5 = new Card(CardSuit.S, CardValue._5);
+    public static final Card CARD_S6 = new Card(CardSuit.S, CardValue._6);
+    public static final Card CARD_S7 = new Card(CardSuit.S, CardValue._7);
+    public static final Card CARD_S8 = new Card(CardSuit.S, CardValue._8);
+    public static final Card CARD_S9 = new Card(CardSuit.S, CardValue._9);
+    public static final Card CARD_ST = new Card(CardSuit.S, CardValue._T);
+    public static final Card CARD_SJ = new Card(CardSuit.S, CardValue._J);
+    public static final Card CARD_SQ = new Card(CardSuit.S, CardValue._Q);
+    public static final Card CARD_SK = new Card(CardSuit.S, CardValue._K);
+    public static final Card CARD_SA = new Card(CardSuit.S, CardValue._A);
+
+}
